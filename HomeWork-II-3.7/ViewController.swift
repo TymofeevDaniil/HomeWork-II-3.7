78c1adf33a6e1395f850e5093087e47acedf33a2
TymofeevDaniil <tymofeevdaniil@gmail.com>
2021-11-29 18:59:01 +0200

    Initial Commit

---
+++ HomeWork-II-3.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-II-3.7
+//
+//  Created by Danil Tymofeev on 29.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
